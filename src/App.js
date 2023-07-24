1ad7ff1d24fa8aa01ead15872292b45b45b9c5c2
mostafaezat <mostafaezzatshaban@gmail.com>
2023-07-24 16:10:49 +0300

    initial push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Main from "./pages/mainlayout/Main";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/single/:id/" element={<Single />}></Route>
+          <Route path="/user" element={<List />}></Route>
+          <Route path="/new" element={<New />}></Route>
+          <Route path="/new/:id/" element={<New />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
